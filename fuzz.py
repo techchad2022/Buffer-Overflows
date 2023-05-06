8c2fefc63a586e314e91d61ab47e2ad5cc12000a
techchad2022 <104576580+techchad2022@users.noreply.github.com>
2023-05-06 15:32:17 +0545

    Added Comment for timeout
    timeout

--- fuzz.py
+++ fuzz.py
@@ -19,7 +19,7 @@ while True: #Runs continuously until crash
 	try:
 		with socket.socket() as s: #context manager for our socket
 			s.connect((ip,port)) #connect to Windows machine's vulnerable service
-			s.settimeout(4)
+			s.settimeout(4) #automatically stops fuzzing when crash bytes are found
 			print("sending  {} bytes".format(len(string))) #tell us what is being sent
 			s.send(bytes(string,'latin-1')) #sent the string, as bytes from latin ascii
 			reply = s.recv(4096) #see what the reply is
